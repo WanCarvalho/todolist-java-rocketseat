087aed4c19cfbc6a2eed18c14567b12254d4b958
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2023-10-14 02:41:11 -0300

    add: retorno de status http com response entity

--- src/main/java/br/com/wancarvalho/todolist/user/UserController.java
+++ src/main/java/br/com/wancarvalho/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.wancarvalho.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +16,19 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
 
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            System.out.println("Usuário já existe");
-            return null;
+            // retorna status de erro caso usuário já esteja cadastrado
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
         var userCreated = this.userRepository.save(userModel);
 
-        return userCreated;
+        // retorna status de sucesso do tipo 'CREATED' caso o usuário tenha sido
+        // adicionado ao banco
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
