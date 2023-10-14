c3607cddc96ecf7233e2370ac9694d41f4fe986a
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2023-10-14 02:27:47 -0300

    initial commit: arquivos iniciais

---
+++ src/main/java/br/com/wancarvalho/todolist/user/UserModel.java
@@ -0,0 +1,30 @@
+package br.com.wancarvalho.todolist.user;
+
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data // biblioteca lombok adiciona os getters e setters a classe
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    // @Column(name = "usuario") utiliza Anottation caso queira alterar nome da
+    // coluna no banco de dados
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
