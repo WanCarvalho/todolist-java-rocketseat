e37dd4c79f4eaa8e91b244f79fc1ff27ecca068b
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2023-10-14 23:34:05 -0300

    add: criptografia para senha do usuário com a lib bcrypt

--- src/main/java/br/com/wancarvalho/todolist/user/UserController.java
+++ src/main/java/br/com/wancarvalho/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,6 +26,11 @@ public class UserController {
             // retorna status de erro caso usuário já esteja cadastrado
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        // criptografar senha antes de mandar para banco de dados
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
 
